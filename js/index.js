ab0fc4de7c5d767dc3e10c1d28d37571d4c5979d
voloshyyn <25062006vas@gmail.com>
2024-09-03 22:13:51 +0300

    Added js scroll

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function init() {
     import('./global/global.header-nav.burger-menu.js');
     import('./index/index.best-selling.carousel.js');
-   // import('./index/index-turnkey-furnishing-scroll.js');
+    import('./index/index-turnkey-furnishing-scroll.js');
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
